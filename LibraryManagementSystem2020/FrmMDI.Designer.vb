82498173fdce39bb094aa1dedde42b0d2a33c166
abbas_empire <abbasmaan1886@gmail.com>
2020-03-26 19:30:10 +0530

    adding icon on top of the form

--- LibraryManagementSystem2020/FrmMDI.Designer.vb
+++ LibraryManagementSystem2020/FrmMDI.Designer.vb
@@ -22,6 +22,7 @@ Partial Class FrmMDI
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMDI))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.InventoryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NewBookToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -50,65 +51,68 @@ Partial Class FrmMDI
         '
         Me.InventoryToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewBookToolStripMenuItem})
         Me.InventoryToolStripMenuItem.Name = "InventoryToolStripMenuItem"
-        Me.InventoryToolStripMenuItem.Size = New System.Drawing.Size(73, 22)
+        Me.InventoryToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
         Me.InventoryToolStripMenuItem.Text = "&Inventory"
         '
         'NewBookToolStripMenuItem
         '
         Me.NewBookToolStripMenuItem.Name = "NewBookToolStripMenuItem"
-        Me.NewBookToolStripMenuItem.Size = New System.Drawing.Size(134, 22)
+        Me.NewBookToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.NewBookToolStripMenuItem.Text = "&New Book"
         '
         'TransactionToolStripMenuItem
         '
         Me.TransactionToolStripMenuItem.Name = "TransactionToolStripMenuItem"
-        Me.TransactionToolStripMenuItem.Size = New System.Drawing.Size(86, 22)
+        Me.TransactionToolStripMenuItem.Size = New System.Drawing.Size(80, 20)
         Me.TransactionToolStripMenuItem.Text = "&Transaction"
         '
         'ReportsToolStripMenuItem
         '
         Me.ReportsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewMemberToolStripMenuItem})
         Me.ReportsToolStripMenuItem.Name = "ReportsToolStripMenuItem"
-        Me.ReportsToolStripMenuItem.Size = New System.Drawing.Size(71, 22)
+        Me.ReportsToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
         Me.ReportsToolStripMenuItem.Text = "&Members"
         '
         'NewMemberToolStripMenuItem
         '
         Me.NewMemberToolStripMenuItem.Name = "NewMemberToolStripMenuItem"
-        Me.NewMemberToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.NewMemberToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.NewMemberToolStripMenuItem.Text = "&New Member"
         '
         'HelpToolStripMenuItem
         '
         Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CreateStaffToolStripMenuItem})
         Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
-        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(45, 22)
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
         Me.HelpToolStripMenuItem.Text = "&Staff"
         '
         'CreateStaffToolStripMenuItem
         '
         Me.CreateStaffToolStripMenuItem.Name = "CreateStaffToolStripMenuItem"
-        Me.CreateStaffToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
+        Me.CreateStaffToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
         Me.CreateStaffToolStripMenuItem.Text = "&Create Staff"
         '
         'ReportToolStripMenuItem
         '
         Me.ReportToolStripMenuItem.Name = "ReportToolStripMenuItem"
-        Me.ReportToolStripMenuItem.Size = New System.Drawing.Size(58, 22)
+        Me.ReportToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
         Me.ReportToolStripMenuItem.Text = "&Report"
         '
         'HelpToolStripMenuItem1
         '
         Me.HelpToolStripMenuItem1.Name = "HelpToolStripMenuItem1"
-        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(46, 22)
+        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(44, 20)
         Me.HelpToolStripMenuItem1.Text = "&Help"
         '
         'FrmMDI
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.MenuStrip1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.IsMdiContainer = True
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "FrmMDI"
